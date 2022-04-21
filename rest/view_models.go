48852f6893a8bd735623359489d13db79c0285bf
Bogdan Brinza <Bogdan.Brinza@gmail.com>
2022-04-21 11:42:27 -0700

    Hiding store tags property if there is nothing to display
    Starting to refactor code to move OS downloads processing to server

--- rest/view_models.go
+++ rest/view_models.go
@@ -4,10 +4,7 @@ import (
 	"fmt"
 	"github.com/arelate/vangogh_local_data"
 	"html/template"
-	"net/url"
-	"path"
 	"sort"
-	"strings"
 )
 
 type listProductViewModel struct {
@@ -114,70 +111,6 @@ func listViewModelFromRedux(order []string, redux map[string]map[string][]string
 		lvm.Products = append(lvm.Products, lpvm)
 	}
 	return lvm
-}
-
-func rewriteDescriptionItemsLinks(desc string) string {
-
-	itemsUrls := vangogh_local_data.ExtractDescItems(desc)
-
-	for _, itemUrl := range itemsUrls {
-		if u, err := url.Parse(itemUrl); err != nil {
-			continue
-		} else {
-			ggUrl := "/items" + u.Path
-			desc = strings.Replace(desc, itemUrl, ggUrl, -1)
-		}
-	}
-
-	return desc
-}
-
-func rewriteDescriptionGameLinks(desc string) string {
-	gameLinks := vangogh_local_data.ExtractGameLinks(desc)
-
-	for _, gameLink := range gameLinks {
-		if u, err := url.Parse(gameLink); err != nil {
-			continue
-		} else {
-			_, slug := path.Split(u.Path)
-			ggUrl := "/product?slug=" + slug
-			desc = strings.Replace(desc, gameLink, ggUrl, -1)
-		}
-	}
-
-	return desc
-}
-
-const doubleNewLineChar = "\n\n"
-const newLineChar = "\n"
-const emDashCode = "\u2013"
-
-//implicitToExplicitList looks for embedded characters
-//that GOG.com is using for <ul> lists creation, e.g.
-//https://www.gog.com/en/game/deaths_gambit
-//and replaces that segment with explicit unordered lists.
-//Currently known characters are listed as consts above this func.
-func implicitToExplicitList(text string) string {
-	var items []string
-	if strings.Contains(text, doubleNewLineChar) {
-		items = strings.Split(text, doubleNewLineChar)
-	} else if strings.Contains(text, newLineChar) {
-		items = strings.Split(text, newLineChar)
-	} else if strings.Contains(text, emDashCode) {
-		items = strings.Split(text, emDashCode)
-	}
-
-	if len(items) > 0 {
-		builder := strings.Builder{}
-		builder.WriteString("<ul>")
-		for _, item := range items {
-			builder.WriteString("<li>" + item + "</li>")
-		}
-		builder.WriteString("</ul>")
-		text = builder.String()
-	}
-
-	return text
 }
 
 func productViewModelFromRedux(redux map[string]map[string][]string) (*productViewModel, error) {
