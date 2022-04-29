f02f26e4416c9b81222a8e3c81c0296fc2ec400e
Bogdan Brinza <Bogdan.Brinza@gmail.com>
2022-04-28 21:45:12 -0700

    Implementing top level link shortcuts for meaningful browser link tooltips
    Fixing mobile extra width leading to undesired horizontal scroll

--- rest/routing.go
+++ rest/routing.go
@@ -18,6 +18,24 @@ func HandleFuncs() {
 		"/files":       basicHttpAuth(nod.RequestLog(http.HandlerFunc(GetFiles))),
 		"/local-file/": basicHttpAuth(nod.RequestLog(http.HandlerFunc(GetLocalFile))),
 		"/favicon.ico": http.HandlerFunc(http.NotFound),
+
+		// updates redirects
+		"/updates/recent":    http.RedirectHandler("/updates?since=4", http.StatusPermanentRedirect),
+		"/updates/today":     http.RedirectHandler("/updates?since=24", http.StatusPermanentRedirect),
+		"/updates/this_week": http.RedirectHandler("/updates?since=120", http.StatusPermanentRedirect),
+
+		// products redirects
+		"/products/downloads": http.RedirectHandler(
+			"/search?scope=downloads&types=account-products&sort=gog-order-date&desc=true",
+			http.StatusPermanentRedirect),
+		"/products/wishlist": http.RedirectHandler(
+			"/search?scope=wishlist&wishlisted=true&sort=rating&desc=true",
+			http.StatusPermanentRedirect),
+		"/products/store": http.RedirectHandler(
+			"/search?scope=store&types=store-products&sort=gog-release-date&desc=true",
+			http.StatusPermanentRedirect),
+		"/products": http.RedirectHandler("/search", http.StatusPermanentRedirect),
+
 		// start at the account
 		"/": http.RedirectHandler("/updates", http.StatusPermanentRedirect),
 	}
