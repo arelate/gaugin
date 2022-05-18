aa9de1df49b7dab07134078c3f9a3e6025f7841c
Bogdan Brinza <Bogdan.Brinza@gmail.com>
2022-05-18 12:15:18 -0700

    Setting CSP script-src to none

--- rest/default-headers.go
+++ rest/default-headers.go
@@ -4,5 +4,5 @@ import "net/http"
 
 func defaultHeaders(w http.ResponseWriter) {
 	w.Header().Set("Content-Type", "text/html")
-	w.Header().Set("Content-Security-Policy", "default-src 'self'; style-src 'unsafe-inline';")
+	w.Header().Set("Content-Security-Policy", "default-src 'self'; script-src: 'none'; style-src 'unsafe-inline';")
 }
