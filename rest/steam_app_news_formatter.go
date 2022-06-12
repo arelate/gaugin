748650eef6154c44ebbe8d2c8a6190c581b217f3
Bogdan Brinza <Bogdan.Brinza@gmail.com>
2022-06-12 12:47:58 -0700

    Reformatting links in Steam app news
    Tweaking linke break to use opportunistic break

--- rest/steam_app_news_formatter.go
+++ rest/steam_app_news_formatter.go
@@ -1,34 +1,51 @@
 package rest
 
-import "strings"
+import (
+	"regexp"
+	"strings"
+)
 
 var formatters = map[string]string{
-	"[b]":     "<b>",
-	"[/b]":    "</b>",
-	"[u]":     "<u>",
-	"[/u]":    "</u>",
-	"[i]":     "<i>",
-	"[/i]":    "</i>",
-	"[h1]":    "<h1>",
-	"[/h1]":   "</h1>",
-	"[h2]":    "<h2>",
-	"[/h2]":   "</h2>",
-	"[h3]":    "<h3>",
-	"[/h3]":   "</h3>",
-	"[list]":  "<ul>",
-	"[*]":     "<li>",
-	"[/list]": "</ul>",
-	"\n":      "<br>",
-	//since CSP will not allow images from another host - converting images to links for now
-	"[img]":              "<a target='_top' href='",
-	"[/img]":             "'>External image</a>",
+	"[b]":                "<b>",
+	"[/b]":               "</b>",
+	"[u]":                "<u>",
+	"[/u]":               "</u>",
+	"[i]":                "<i>",
+	"[/i]":               "</i>",
+	"[h1]":               "<h1>",
+	"[/h1]":              "</h1>",
+	"[h2]":               "<h2>",
+	"[/h2]":              "</h2>",
+	"[h3]":               "<h3>",
+	"[/h3]":              "</h3>",
+	"[list]":             "<ul>",
+	"[*]":                "<li>",
+	"[/list]":            "</ul>",
+	"\n":                 "<wbr>",
 	"{STEAM_CLAN_IMAGE}": "https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/clans/",
+	"[img]":              "<img src='",
+	"[/img]":             "'/>",
 }
 
 func steamAppNewsToHTML(c string) string {
+
+	urls := extractUrls(c)
+	for _, u := range urls {
+		nu := strings.Replace(u, "[url=", "<a target='_top' href='", 1)
+		nu = strings.Replace(nu, "]", "'>", 1)
+		nu = strings.Replace(nu, "[/url]", "</a>", 1)
+		c = strings.Replace(c, u, nu, 1)
+	}
+
 	for t := range formatters {
 		c = strings.Replace(c, t, formatters[t], -1)
 	}
 
 	return c
+}
+
+var steamNewsItemUrl = regexp.MustCompile(`\[url=[\w:/*-. ]*][\w:\/\[\]*-. ]*?\[/url]`)
+
+func extractUrls(c string) []string {
+	return steamNewsItemUrl.FindAllString(c, -1)
 }
