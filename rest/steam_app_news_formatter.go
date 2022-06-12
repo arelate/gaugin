4d441c49d49c199224956f3db9ffa7eed0db1ad3
Bogdan Brinza <Bogdan.Brinza@gmail.com>
2022-06-11 20:29:51 -0700

    Breaking lines in Steam app news

--- rest/steam_app_news_formatter.go
+++ rest/steam_app_news_formatter.go
@@ -3,23 +3,24 @@ package rest
 import "strings"
 
 var formatters = map[string]string{
-	"[b]":                "<b>",
-	"[/b]":               "</b>",
-	"[u]":                "<u>",
-	"[/u]":               "</u>",
-	"[h1]":               "<h1>",
-	"[/h1]":              "</h1>",
-	"[h2]":               "<h2>",
-	"[/h2]":              "</h2>",
-	"[h3]":               "<h3>",
-	"[/h3]":              "</h3>",
-	"[list]":             "<ul>",
-	"[*]":                "<li>",
-	"[/list]":            "</ul>",
-	"{STEAM_CLAN_IMAGE}": "https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/clans/",
+	"[b]":     "<b>",
+	"[/b]":    "</b>",
+	"[u]":     "<u>",
+	"[/u]":    "</u>",
+	"[h1]":    "<h1>",
+	"[/h1]":   "</h1>",
+	"[h2]":    "<h2>",
+	"[/h2]":   "</h2>",
+	"[h3]":    "<h3>",
+	"[/h3]":   "</h3>",
+	"[list]":  "<ul>",
+	"[*]":     "<li>",
+	"[/list]": "</ul>",
+	"\n":      "<br>",
 	//since CSP will not allow images from another host - converting images to links for now
-	"[img]":  "<a target='_top' href='",
-	"[/img]": "'>External image</a>",
+	"[img]":              "<a target='_top' href='",
+	"[/img]":             "'>External image</a>",
+	"{STEAM_CLAN_IMAGE}": "https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/clans/",
 }
 
 func steamAppNewsToHTML(c string) string {
