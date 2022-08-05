5e1333e2dc6af744c208a0600f111885f904e055
Bogdan Brinza <Bogdan.Brinza@gmail.com>
2022-08-05 15:42:09 -0700

    Replacing remaining icons - expand/collapse and back to top
    Sadly there are two Safari bugs/gaps preventing great experience here:
    
    - Safari doesn't support SVG path d as a presentational attribute. Ideally, expand/collapse should be single icon that switches d via CSS for open/closed state (perhaps even animated)
    
    - Back to top doesn't work in Safari in product details (works in updates). There is no script - it's just a link that doesn't work in Safari

--- rest/get-downloads.go
+++ rest/get-downloads.go
@@ -10,7 +10,7 @@ import (
 )
 
 var downloadTypeTitles = map[vangogh_local_data.DownloadType]string{
-	vangogh_local_data.Installer: "Setup",
+	vangogh_local_data.Installer: "Installer",
 	vangogh_local_data.DLC:       "DLC",
 	vangogh_local_data.Extra:     "Extra",
 	vangogh_local_data.Movie:     "Movie",
