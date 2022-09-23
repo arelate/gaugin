3f3a6397cb6871eace0f2371ea92f2379c92a25d
Bogdan Brinza <Bogdan.Brinza@gmail.com>
2022-09-22 20:51:49 -0700

    Bumping stencil to get smooth image loading
    Cleaning up CSP a bit

--- gaugin_middleware/default-headers.go
+++ gaugin_middleware/default-headers.go
@@ -1,21 +1,14 @@
 package gaugin_middleware
 
 import (
+	"github.com/boggydigital/stencil"
 	"net/http"
+	"strings"
 )
 
 const (
 	htmlContentType = "text/html"
 	defaultCSP      = "default-src 'self'; " +
-		"script-src " +
-		//script-product-details.gohtml
-		"'sha256-EoiesIg5jhsIaHn7PSaZ/oT9Yi0MCUx9WzALOyH9HkE=' " +
-		//script-iframe.gohtml
-		"'sha256-vEdzDTUjeRFG21L/pW+qldt1k+gnTSWl4v2E16iqJPc=' " +
-		//script-missing-images.gohtml
-		"'sha256-vAj29f8vCI+Qq0UyQqV6YB/RcBCG4wjrOhqmemg5uoU=' " +
-		//script-image-fade-in.gothml
-		"'sha256-0o6kCVC1+8tieHTMZdKurbNC2fDP/bQEO5AAmFyRqBo=' " +
 		"'unsafe-inline'; " +
 		"object-src 'none'; " +
 		"img-src 'self' data:; " +
@@ -24,7 +17,11 @@ const (
 
 func DefaultHeaders(st *ServerTimings, w http.ResponseWriter) {
 	w.Header().Set("Content-Type", htmlContentType)
-	w.Header().Set("Content-Security-Policy", defaultCSP)
+	stencilCSP := defaultCSP + "script-src " + strings.Join(stencil.ScriptHashes, " ") +
+		//script-image-fade-in.gothml
+		" 'sha256-0o6kCVC1+8tieHTMZdKurbNC2fDP/bQEO5AAmFyRqBo=' "
+
+	w.Header().Set("Content-Security-Policy", stencilCSP)
 	if st != nil {
 		w.Header().Add("Server-Timing", st.String())
 	}
