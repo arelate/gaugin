676e19cdc1a5ee3ca924b8a45652157874f612ad
Bogdan Brinza <Bogdan.Brinza@hey.com>
2023-12-26 18:32:52 -0800

    Add game title to Steam Deck compatibility report

--- rest/get_steam_deck.go
+++ rest/get_steam_deck.go
@@ -4,6 +4,7 @@ import (
 	"github.com/arelate/gaugin/gaugin_middleware"
 	"github.com/arelate/gaugin/stencil_app"
 	"github.com/arelate/gaugin/view_models"
+	"github.com/arelate/vangogh_local_data"
 	"github.com/boggydigital/nod"
 	"net/http"
 	"strings"
@@ -21,8 +22,19 @@ func GetSteamDeck(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	titleResult, _, err := getRedux(http.DefaultClient, id, false, vangogh_local_data.TitleProperty)
+	if err != nil {
+		http.Error(w, nod.Error(err).Error(), http.StatusInternalServerError)
+		return
+	}
+
+	title := ""
+	if tt, ok := titleResult[id][vangogh_local_data.TitleProperty]; ok && len(tt) > 0 {
+		title = tt[0]
+	}
+
 	sb := &strings.Builder{}
-	sdvm := view_models.NewSteamDeck(sdacr)
+	sdvm := view_models.NewSteamDeck(title, sdacr)
 
 	if err := tmpl.ExecuteTemplate(sb, "steam-deck-content", sdvm); err != nil {
 		http.Error(w, nod.Error(err).Error(), http.StatusInternalServerError)
--- view_models/steam_deck.go
+++ view_models/steam_deck.go
@@ -1,16 +1,20 @@
 package view_models
 
 import (
+	"fmt"
 	"github.com/arelate/southern_light/steam_integration"
 	"html/template"
 )
 
-var messageByCategory = map[string]template.HTML{
-	"Verified": "Valve’s testing indicates that this game is <span class='category verified'>Verified</span> on Steam Deck. " +
+var messageByCategory = map[string]string{
+	"Verified": "Valve’s testing indicates that <span class='title'>%s</span> is " +
+		"<span class='category verified'>Verified</span> on Steam Deck. " +
 		"This game is fully functional on Steam Deck, and works great with the built-in controls and display.",
-	"Playable": "Valve’s testing indicates that this game is <span class='category playable'>Playable</span> on Steam Deck. " +
+	"Playable": "Valve’s testing indicates that <span class='title'>%s</span> is " +
+		"<span class='category playable'>Playable</span> on Steam Deck. " +
 		"This game is functional on Steam Deck, but might require extra effort to interact with or configure.",
-	"Unsupported": "Valve’s testing indicates that 8-Bit Hordes is <span class='category unsupported'>Unsupported</span> on Steam Deck. " +
+	"Unsupported": "Valve’s testing indicates that <span class='title'>%s</span> is " +
+		"<span class='category unsupported'>Unsupported</span> on Steam Deck. " +
 		"Some or all of this game currently doesn't function on Steam Deck.",
 	"Unknown": "Unknown",
 }
@@ -22,9 +26,11 @@ type steamDeck struct {
 	Results            []string
 }
 
-func NewSteamDeck(dacr *steam_integration.DeckAppCompatibilityReport) *steamDeck {
+func NewSteamDeck(title string, dacr *steam_integration.DeckAppCompatibilityReport) *steamDeck {
+	message := template.HTML(fmt.Sprintf(messageByCategory[dacr.String()], title))
+
 	return &steamDeck{
-		Message:            messageByCategory[dacr.String()],
+		Message:            message,
 		BlogUrl:            dacr.GetSteamDeckBlogUrl(),
 		ResultsDisplayType: dacr.GetResultsDisplayTypes(),
 		Results:            dacr.GetResults(),
