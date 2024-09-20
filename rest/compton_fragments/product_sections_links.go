27f8b2a013bec4a0e16115f446eea8b99e02ce11
Bogdan Brinza <Bogdan.Brinza@hey.com>
2024-09-19 22:53:09 -0700

    Bumping compton
    Reimplementing section links as an expandable section

--- rest/compton_fragments/product_sections_links.go
+++ rest/compton_fragments/product_sections_links.go
@@ -3,21 +3,45 @@ package compton_fragments
 import (
 	"github.com/arelate/gaugin/rest/compton_data"
 	"github.com/boggydigital/compton"
-	"github.com/boggydigital/compton/elements/nav_links"
+	"github.com/boggydigital/compton/consts/align"
+	"github.com/boggydigital/compton/consts/color"
+	"github.com/boggydigital/compton/consts/direction"
+	"github.com/boggydigital/compton/consts/size"
+	"github.com/boggydigital/compton/consts/weight"
+	"github.com/boggydigital/compton/elements/els"
+	"github.com/boggydigital/compton/elements/flex_items"
+	"github.com/boggydigital/compton/elements/fspan"
+	"github.com/boggydigital/compton/elements/section"
 )
 
 func ProductSectionsLinks(r compton.Registrar, sections []string) compton.Element {
 
-	productSectionsLinks := make(map[string]string)
-	for _, section := range sections {
-		title := compton_data.SectionTitles[section]
-		productSectionsLinks[title] = "#" + title
+	linksSection := section.Section(r).
+		BackgroundColor(color.Highlight).
+		FontSize(size.Small).
+		FontWeight(weight.Normal)
+
+	span := fspan.Text(r, "Scroll to...").
+		FontSize(size.Normal).
+		FontWeight(weight.Bolder)
+	ds := els.Details().AppendSummary(span)
+	ds.AddClass("product-sections-links")
+
+	linksStack := flex_items.FlexItems(r, direction.Row).
+		JustifyContent(align.Center).
+		RowGap(size.Small)
+
+	ds.Append(linksStack)
+	linksSection.Append(ds)
+
+	for _, s := range sections {
+		title := compton_data.SectionTitles[s]
+		link := els.A("#" + title)
+		linkText := fspan.Text(r, title).
+			ForegroundColor(color.LightBlue)
+		link.Append(linkText)
+		linksStack.Append(link)
 	}
 
-	targets := nav_links.TextLinks(
-		productSectionsLinks,
-		"",
-		compton_data.SectionsTitlesOrder...)
-
-	return nav_links.NavLinksTargets(r, targets...)
+	return linksSection
 }
