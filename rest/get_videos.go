2008d10c804ced42465fde7eb8a9d26bda7a398f
Bogdan Brinza <Bogdan.Brinza@hey.com>
2024-09-20 17:07:20 -0700

    Refactoring external video links to be displayed along with the video

--- rest/get_videos.go
+++ rest/get_videos.go
@@ -4,9 +4,9 @@ import (
 	"github.com/arelate/gaugin/rest/compton_data"
 	"github.com/arelate/gaugin/rest/gaugin_styles"
 	"github.com/arelate/vangogh_local_data"
+	"github.com/boggydigital/compton"
 	"github.com/boggydigital/compton/consts/color"
 	"github.com/boggydigital/compton/consts/direction"
-	"github.com/boggydigital/compton/elements/details_summary"
 	"github.com/boggydigital/compton/elements/els"
 	"github.com/boggydigital/compton/elements/flex_items"
 	"github.com/boggydigital/compton/elements/fspan"
@@ -35,30 +35,11 @@ func GetVideos(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	mvRedux, err := getRedux(
-		http.DefaultClient,
-		"",
-		true,
-		vangogh_local_data.MissingVideoUrlProperty)
-
-	if err != nil {
-		http.Error(w, nod.Error(err).Error(), http.StatusInternalServerError)
-		return
-	}
-
-	rdx := kevlar.ReduxProxy(MergeIdPropertyValues(idRedux, mvRedux))
+	rdx := kevlar.ReduxProxy(idRedux)
 
 	videoIds := make([]string, 0)
 	if vp, ok := rdx.GetAllValues(vangogh_local_data.VideoIdProperty, id); ok && len(vp) > 0 {
 		videoIds = vp
-	}
-
-	localVideos := make([]string, 0, len(videoIds))
-
-	for _, videoId := range videoIds {
-		if !rdx.HasKey(vangogh_local_data.MissingVideoUrlProperty, videoId) {
-			localVideos = append(localVideos, videoId)
-		}
 	}
 
 	section := compton_data.VideosSection
@@ -74,39 +55,29 @@ func GetVideos(w http.ResponseWriter, r *http.Request) {
 		pageStack.Append(recipes.Center(ifc, fs))
 	}
 
-	for _, videoId := range localVideos {
-		src := "/video?id=" + videoId
-		posterSrc := "/thumbnails?id=" + videoId
-		video := els.Video(src)
-		video.SetAttribute("controls", "")
-		video.SetAttribute("poster", posterSrc)
-		pageStack.Append(video)
-	}
-
-	if len(videoIds) > 0 {
-
-		detailsSummary := details_summary.
-			Closed(ifc, "Watch on YouTube").
-			ForegroundColor(color.Cyan)
-
-		dsStack := flex_items.FlexItems(ifc, direction.Column)
-		detailsSummary.Append(dsStack)
-
-		for _, videoId := range videoIds {
-			posterSrc := "/thumbnails?id=" + videoId
-			originSrc := youtube_urls.VideoUrl(videoId).String()
-			link := els.A(originSrc)
-			link.SetAttribute("target", "_top")
-			img := els.ImageLazy(posterSrc)
-			img.SetAttribute("alt", "VideoId: "+videoId)
-			link.Append(img)
-			dsStack.Append(link)
-		}
-
-		pageStack.Append(detailsSummary)
+	for _, videoId := range videoIds {
+		pageStack.Append(createVideo(ifc, videoId))
 	}
 
 	if err := ifc.WriteContent(w); err != nil {
 		http.Error(w, nod.Error(err).Error(), http.StatusInternalServerError)
 	}
+}
+
+func createVideo(r compton.Registrar, videoId string) compton.Element {
+	src := "/video?id=" + videoId
+	posterSrc := "/thumbnails?id=" + videoId
+
+	stack := flex_items.FlexItems(r, direction.Column)
+
+	video := els.Video(src)
+	video.SetAttribute("controls", "")
+	video.SetAttribute("poster", posterSrc)
+	stack.Append(video)
+
+	originLink := els.AText("Watch at origin", youtube_urls.VideoUrl(videoId).String())
+	originLink.AddClass("external")
+	stack.Append(originLink)
+
+	return stack
 }
